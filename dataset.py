eedf5900672cb1d385e664c4cc48766d1fdd4910
wyl516w <wyl21w@126.com>
2024-11-04 09:52:48 +0800

    1

---
+++ dataset.py
@@ -0,0 +1,59 @@
+import torch
+import pytorch_lightning as pl
+import glob
+import os
+from torchvision.io import read_video
+
+class LRW_DataModule(pl.LightningDataModule):
+    def __init__(self, batch_size=32, path="path/to/LRW", num_workers=0):
+        super(LRW_DataModule, self).__init__()
+        self.batch_size = batch_size
+        self.path = path
+        self.num_workers = num_workers
+
+    def setup(self, stage=None):
+        self.train_dataset = self.get_dataset(self.path, "train")
+        self.val_dataset = self.get_dataset(self.path, "val")
+        self.test_dataset = self.get_dataset(self.path, "test")
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def get_dataset(self, path, mode):
+        # path is "path/lipread_mp4/{word}/{mode}/{word}_{index}.mp4"
+        # glob path, end with .mp4
+        glob_path = os.path.join(path, "lipread_mp4", "*", mode, "*.mp4")
+        files_list = glob.glob(glob_path)
+
+        class Dataset(torch.utils.data.Dataset):
+            def __init__(self, files_list):
+                self.files_list = files_list
+
+            def __len__(self):
+                return len(self.files_list)
+
+            def __getitem__(self, index):
+                filename = self.files_list[index]
+                # read video and audio
+                file = read_video(filename)
+                video, audio, _ = file
+                video = video.permute(0, 3, 1, 2).float()
+                audio = audio.float()
+                return video, audio
+
+        return Dataset(files_list)
--- model.py
+++ model.py
@@ -3,9 +3,37 @@ from torch import nn
 import torchvision
 import torch.nn.functional as F
 import pytorch_lightning as pl
-import glob
-import os
-from torchvision.io import read_video
+from .dataset import LRW_DataModule
+
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        super(VectorQuantizer, self).__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.commitment_cost = commitment_cost
+        self.embeddings = nn.Embedding(num_embeddings, embedding_dim)
+    def forward(self, x):
+        flat_input = x.view(-1, self.embedding_dim)
+        distances = (
+            torch.sum(flat_input**2, dim=1, keepdim=True)
+            + torch.sum(self.embeddings.weight**2, dim=1)
+            - 2 * torch.matmul(flat_input, self.embeddings.weight.t())
+        )
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(
+            encoding_indices.size(0), self.num_embeddings, device=x.device
+        )
+        encodings.scatter_(1, encoding_indices, 1)
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self.embeddings.weight).view(x.shape)
+        # Straight Through Estimator
+        quantized = x + (quantized - x).detach()
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), x)
+        q_latent_loss = F.mse_loss(quantized, x.detach())
+        loss = q_latent_loss + self.commitment_cost * e_latent_loss
+        return quantized, loss, encoding_indices
 
 
 class Picture2Feature(nn.Module):
@@ -21,21 +49,41 @@ class Picture2Feature(nn.Module):
 class Feature2Classifier(nn.Module):
     def __init__(self, num_classes=32, num_features=512, num_hidden=256):
         super(Feature2Classifier, self).__init__()
-        self.classifer = nn.Sequential(nn.Linear(num_features, num_hidden), nn.ReLU(), nn.Linear(num_hidden, num_classes))
+        self.classifer = nn.Sequential(
+            nn.Linear(num_features, num_hidden),
+            nn.ReLU(),
+            nn.Linear(num_hidden, num_classes),
+        )
 
     def forward(self, x):
         return self.classifer(x)
 
 
 class Feature2Audio(nn.Module):
-    def __init__(self, video_length=29, video_feature=512, audio_length=16000, audio_feature=1):
+    def __init__(
+        self, video_length=29, video_feature=512, audio_length=16000, audio_feature=1
+    ):
         super(Feature2Audio, self).__init__()
         self.preconv = nn.Sequential(
-            nn.ConvTranspose1d(video_feature, video_feature // 2, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose1d(
+                video_feature, video_feature // 2, kernel_size=3, stride=2, padding=1
+            ),
             nn.ReLU(),
-            nn.ConvTranspose1d(video_feature // 2, video_feature // 4, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose1d(
+                video_feature // 2,
+                video_feature // 4,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+            ),
             nn.ReLU(),
-            nn.ConvTranspose1d(video_feature // 4, video_feature // 8, kernel_size=3, stride=2, padding=1),
+            nn.ConvTranspose1d(
+                video_feature // 4,
+                video_feature // 8,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+            ),
             nn.ReLU(),
         )
         self.mid_length = ((video_length * 2 - 1) * 2 - 1) * 2 - 1
@@ -59,40 +107,50 @@ class Feature2Audio(nn.Module):
 
 
 class Model(pl.LightningModule):
-    def __init__(self, num_classes=32, num_features=512, num_hidden=256, video_length=29, audio_length=16000):
+    def __init__(
+        self,
+        num_classes=32,
+        num_features=512,
+        num_hidden=256,
+        video_length=29,
+        audio_length=16000,
+    ):
         super(Model, self).__init__()
         self.picture2feature = Picture2Feature()
-        self.feature2classifier = Feature2Classifier(num_classes=num_classes, num_features=num_features, num_hidden=num_hidden)
-        self.feature2audio = Feature2Audio(video_length=video_length, video_feature=num_features, audio_length=audio_length, audio_feature=1)
+        self.embedding = VectorQuantizer(
+            num_embeddings=num_classes, embedding_dim=num_features, commitment_cost=0.25
+        )
+        self.feature2audio = Feature2Audio(
+            video_length=video_length,
+            video_feature=num_features,
+            audio_length=audio_length,
+            audio_feature=1,
+        )
         self.num_features = num_features
 
     def forward(self, x):
         b, t, c, h, w = x.shape
         x = x.reshape(b * t, c, h, w)
         feature = self.picture2feature(x)
-        output_classifier = self.feature2classifier(feature)
         feature = feature.reshape(b, t, self.num_features)
-        output_audio = self.feature2audio(feature)
-        return output_classifier, output_audio
+        quantized, vq_loss, _ = self.embedding(feature)
+        audio_hat = self.feature2audio(quantized)
+        return audio_hat, feature, vq_loss
 
     def training_step(self, batch, batch_idx):
         video, audio = batch
         # video: [batch, 29, 3, 256, 256], audio: [batch, 19456]
-        classifier, audio_hat = self(video)
-        predict_class = torch.argmax(classifier, dim=1)
-        loss_classifier = F.cross_entropy(classifier, predict_class)
+        audio_hat, feature, vq_loss = self(video)
         loss_audio = F.mse_loss(audio_hat, audio)
-        loss = 0.1 * loss_classifier + loss_audio
+        loss = loss_audio + vq_loss
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         video, audio = batch
         # video: [batch, 29, 3, 256, 256], audio: [batch, 19456]
-        classifier, audio_hat = self(video)
-        predict_class = torch.argmax(classifier, dim=1)
-        loss_classifier = F.cross_entropy(classifier, predict_class)
+        audio_hat, feature, vq_loss = self(video)
         loss_audio = F.mse_loss(audio_hat, audio)
-        loss = 0.1 * loss_classifier + loss_audio
+        loss = loss_audio + vq_loss
         return loss
 
     def configure_optimizers(self):
@@ -102,59 +160,24 @@ class Model(pl.LightningModule):
         avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
         self.log("loss", avg_loss)
 
-    
     def on_training_epoch_end(self, outputs):
         avg_loss = torch.stack([x["loss"] for x in outputs]).mean()
         self.log("loss_val", avg_loss)
 
-class LRW_DataModule(pl.LightningDataModule):
-    def __init__(self, batch_size=32, path="path/to/LRW",num_workers=0):
-        super(LRW_DataModule, self).__init__()
-        self.batch_size = batch_size
-        self.path = path
-        self.num_workers = num_workers
-
-    def setup(self, stage=None):
-        self.train_dataset = self.get_dataset(self.path, "train")
-        self.val_dataset = self.get_dataset(self.path, "val")
-        self.test_dataset = self.get_dataset(self.path, "test")
-
-    def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
-
-    def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
-
-    def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
-
-    def get_dataset(self, path, mode):
-        # path is "path/lipread_mp4/{word}/{mode}/{word}_{index}.mp4"
-        # glob path, end with .mp4
-        glob_path = os.path.join(path, "lipread_mp4", "*", mode, "*.mp4")
-        files_list = glob.glob(glob_path)
-        class Dataset(torch.utils.data.Dataset):
-            def __init__(self, files_list):
-                self.files_list = files_list
-
-            def __len__(self):
-                return len(self.files_list)
-
-            def __getitem__(self, index):
-                filename = self.files_list[index]
-                # read video and audio
-                file = read_video(filename)
-                video, audio, _ = file
-                video = video.permute(0, 3, 1, 2).float()
-                audio = audio.float()
-                return video, audio
-        return Dataset(files_list)
 
 if __name__ == "__main__":
     # LRW video is (29, 3, 256, 256), audio is (1, 19456)
-    model = Model(num_classes=32, num_features=512, num_hidden=256, video_length=29, audio_length=19456)
+    model = Model(
+        num_classes=32,
+        num_features=512,
+        num_hidden=256,
+        video_length=29,
+        audio_length=19456,
+    )
     # dataset is LRW dataset
-    datamodule = LRW_DataModule(path="/data1/wuyilei_dataset/LRW",batch_size=16,num_workers=16)
+    datamodule = LRW_DataModule(
+        path="/data1/wuyilei_dataset/LRW", batch_size=16, num_workers=16
+    )
     trainer = pl.Trainer(max_epochs=10)
     trainer.fit(model, datamodule)
     trainer.test()
